30a3f5300947f4206c6b9a446e7de54dfc9f3e31
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-02 12:36:44 -0400

    -Update like model - Add method action

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,14 @@
 class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
+  after_save :update_likes_counter
+  after_destroy :decrease_likes_counter
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
+
+  def decrease_likes_counter
+    post.decrement!(:likes_counter)
+  end
 end
