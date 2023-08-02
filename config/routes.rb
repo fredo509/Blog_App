12e0c8f19c8cd3aaff6ec24c023904ed044f42bb
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-02 13:14:46 -0400

    - Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :likes
-  resources :posts
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create destroy] do
+      resources :comments, only: %i[new create destroy]
+      resources :likes, only: %i[new create]
+    end 
+  end    
 end
