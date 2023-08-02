cfa61eb24464dca9390fb73ede630f6cde90ee7a
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-02 11:03:19 -0400

    -Update like migration -Add new columns -Add foreign key

--- db/migrate/20230802143836_create_likes.rb
+++ db/migrate/20230802143836_create_likes.rb
@@ -1,8 +1,9 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    create_table :likes do |t|
-
-      t.timestamps
+    create_table :likes do |col|
+      col.references :user, null: false, foreign_key: true
+      col.references :post, null: false, foreign_key: true
+      col.timestamps
     end
   end
 end
