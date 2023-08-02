6434cc9c4c6c00217d9853cd46790ce561c98cc8
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-02 11:01:10 -0400

    - Update post migrattion - Add new columns to table

--- db/migrate/20230802143606_create_posts.rb
+++ db/migrate/20230802143606_create_posts.rb
@@ -1,8 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
-    create_table :posts do |t|
-
-      t.timestamps
+    create_table :posts do |col|
+      col.string :title
+      col.text :text
+      col.integer :comments_count
+      col.integer :likes_count
+      col.references :user, null: false, foreign_key: true
+      col.timestamps
     end
   end
 end
