29aa4cff27bf6784049e1f2fddfd2fee3d7ca0b5
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-02 12:23:18 -0400

    Update comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,17 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
+  after_save :update_comments_number
+  after_destroy :decrease_comments_counter
+
+  validates :text, presence: true, length: { in: 2..100 }
+
+  # A method that updates the comments counter for a post.
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
+
+  def decrease_comments_counter
+    post.decrement!(:comments_counter)
+  end
 end
