387df0bde3d8b4dc1f498b6b03f5708aaf5b7037
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-02 11:31:21 -0400

    - Update post controller -  fix n+1 problem - Implement destroy method for CanCanCan

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,39 @@
 class PostsController < ApplicationController
+  def index
+    @my_user = User.find(params[:user_id])
+    @my_posts = @my_user.posts.includes(:comments, :likes)
+  end
+
+  def show
+    @my_post = Post.find(params[:id])
+  end
+
+  def new
+    @my_post = Post.new
+  end
+
+  def create
+    @my_post = current_user.posts.new(post_params)
+
+    if @my_post.save
+      redirect_to user_path(id: @my_post.author_id), notice: 'Post created :)'
+    else
+      flash.now[:alert] = 'There is something wrong try again.'
+      render :new
+    end
+  end
+
+  def destroy
+    @my_post = Post.find(params[:id])
+    @my_author = @my_post.author
+    @my_author.decrement!(:number_of_post)
+    @my_post.destroy!
+    redirect_to user_posts_path(id: @my_author.id), notice: 'Post removed :)'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
