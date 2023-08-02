0d5ca2a9da68e52cb510f23b4e4344441937bd92
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-02 12:37:12 -0400

    Update like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,9 @@
 class LikesController < ApplicationController
  def create
-    @like = Like.new
-    @like.author = current_user
-    @like.post = Post.find(params[:post_id])
-    @like.save!
+    @i_like = Like.new
+    @i_like.author = current_user
+    @i_like.post = Post.find(params[:post_id])
+    @i_like.save!
     redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id])
   end
 end
