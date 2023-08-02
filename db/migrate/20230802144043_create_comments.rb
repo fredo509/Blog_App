4300ba434f5469e4ef6ecccbc85a3b15d965bca0
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-02 11:05:51 -0400

    - Update comment migration - Add new columns - Add foreign key

--- db/migrate/20230802144043_create_comments.rb
+++ db/migrate/20230802144043_create_comments.rb
@@ -1,8 +1,10 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
-    create_table :comments do |t|
-
-      t.timestamps
+    create_table :comments do |col|
+      col.text :text
+      col.references :user, null: false, foreign_key: true
+      col.references :post, null: false, foreign_key: true
+      col.timestamps
     end
   end
 end
