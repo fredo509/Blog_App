f878553734ed6d228c28d26d7e0c50ee964f6048
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-02 10:57:36 -0400

    -Update migration for Users -Add columns for data

--- db/migrate/20230802143251_create_users.rb
+++ db/migrate/20230802143251_create_users.rb
@@ -1,8 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
-
-      t.timestamps
+    create_table :users do |col|
+      col.string :name
+      col.string :photo
+      col.text :bio
+      col.integer :posts_counter, default: 0
+      col.timestamps
+      col.timestamps
     end
   end
 end
