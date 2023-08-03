61c20e52ec812e83fda21ea3969c6ac0aadbd8e4
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-02 20:06:49 -0400

    update association for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :user, class_name: 'User', foreign_key: :user_id
   belongs_to :post
   after_save :update_comments_number
   after_destroy :decrease_comments_counter
