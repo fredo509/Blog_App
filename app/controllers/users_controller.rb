9a2ba41e550774d01f00dbc141352fd4cde02ca0
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-03 11:08:22 -0400

    Update index method to allow to count post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def index
-    @my_users = User.all
+     @my_users = User.all.includes(:posts)
+    @my_users.each do |user|
+      user.number_of_post = user.posts.count
+    end
   end
 
   def show
     @my_user = User.find(params[:id])
-    @my_user.number_of_post = @user.posts.count
+    @my_user.number_of_post = @my_user.posts.count
   end
 end
