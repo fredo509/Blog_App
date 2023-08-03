5e97a4bfa9fccb4fd97761d09b5fb3cbe22c1dfb
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-03 11:09:50 -0400

    Add method to update count of post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,21 @@
 class Post < ApplicationRecord
-  belongs_to :user
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 
+  def update_author_posts_counter
+    author.update_posts_counter
+  end
+
+  def update_comments_counter
+    update(comments_counter: comments.count)
+  end
+
+  def update_likes_counter
+    update(likes_counter: likes.count)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).includes([:author]).limit(5)
+  end
 end
