3f384725bcc23ed9a11b76a618ea1260f7ba95b1
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-04 09:30:01 -0400

    Pause authenticating for the testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   root 'users#index'
+  # devise_for :users
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create destroy] do
       resources :comments, only: %i[new create destroy]
