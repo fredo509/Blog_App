4819d438c995c68ac5e37c69caf4bac52898e7ba
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-04 09:33:17 -0400

    undo devise for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  before_action :update_allowed_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
+  # before_action :update_allowed_parameters, if: :devise_controller?
 
-  protected
+  # protected
 
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name username bio email password])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name username bio email password])
-  end
+  # def update_allowed_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: %i[name username bio email password])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: %i[name username bio email password])
+  # end
 end
