9a03c2ea08448a0f4de544268e39a444a7fcad7f
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-04 01:10:02 -0400

    Add validate for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def update_author_posts_counter
     author.update_posts_counter
   end
