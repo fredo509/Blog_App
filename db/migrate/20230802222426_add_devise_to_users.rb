840081a54cc6ad4d4a481a4e715fa5ad1cb498dd
Fredo St Fleur <fredostfleur23@gmail.com>
2023-09-21 15:38:00 -0400

    fix duplicate content

--- db/migrate/20230802222426_add_devise_to_users.rb
+++ db/migrate/20230802222426_add_devise_to_users.rb
@@ -4,16 +4,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
       t.string :username,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-
-      ## Rememberable
-      t.datetime :remember_created_at
+     
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -37,10 +29,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       # Uncomment below if timestamps were not included in your original model.
       # t.timestamps null: false
     end
-
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 
